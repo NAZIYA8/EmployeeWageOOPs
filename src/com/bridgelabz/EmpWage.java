59aa65c81a81948266fb18cbe5774c5f122cada2
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-08 21:09:49 +0530

    Added: Calculate wage if employee if present

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -3,17 +3,33 @@ package com.bridgelabz;
 public class EmpWage {
 
     public static void main(String[] args) {
-        ispresent();
+        calculateWage();
     }
 
-    private static void ispresent() {
+    /**
+     * Check if the employee is Present
+     * If Present Salary is returned
+     * If Absent Salary is returned 0
+     */
+    private static void calculateWage() {
         double IS_PRESENT = 1;
         double empCheck = Math.floor(Math.random() * 10) % 2;
 
         if (empCheck == IS_PRESENT) {
-            System.out.println("Employee is Present ");
+            double EmpRatePerHr = 20;
+            double EmpHrs = 8;
+            CalculateSalary(EmpRatePerHr, EmpHrs);
         } else {
-            System.out.println("Employee is Absent");
+            System.out.println("salary is 0");
         }
+    }
+
+    /**
+     * Salary is calculated
+     */
+    private static void CalculateSalary(double EmpRatePerHr, double EmpHrs) {
+        double Salary = EmpRatePerHr * EmpHrs;
+        System.out.println(Salary);
+
     }
 }
