d67f58b58670e569d91b1e0f79f677b60dc9af71
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-08 21:20:55 +0530

    Added: Calculated wages for a month

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -16,28 +16,36 @@ public class EmpWage {
         final int isPartTime = 1;
         final int isFullTime = 2;
         int EmpRatePerHr = 20;
+        int numWorkingHrs = 20;
         int EmpHrs;
+        //int TotalSalary=0;
+        double TotalSalary = 0;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        for (int i = 0; i <= numWorkingHrs; i++) {
 
-        switch ((int) empCheck) {
-            case isPartTime:
-                EmpHrs = 8;
-                break;
-            case isFullTime:
-                EmpHrs = 12;
-                break;
-            default:
-                EmpHrs = 0;
+            switch (empCheck) {
+                case isPartTime:
+                    EmpHrs = 8;
+                    break;
+                case isFullTime:
+                    EmpHrs = 12;
+                    break;
+                default:
+                    EmpHrs = 0;
+            }
+
+            double sum = CalculateSalary(EmpRatePerHr, EmpHrs);
+            TotalSalary = (TotalSalary + sum);
         }
-
-        CalculateSalary(EmpRatePerHr, EmpHrs);
     }
 
     /**
      * Salary is calculated
      */
-    private static void CalculateSalary(double EmpRatePerHr, double EmpHrs) {
+    public static double CalculateSalary(double EmpRatePerHr, double EmpHrs) {
+
         double Salary = EmpRatePerHr * EmpHrs;
         System.out.println(Salary);
+        return Salary;
     }
 }
