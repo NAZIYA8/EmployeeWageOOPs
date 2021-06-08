3e280784e6eeddf18c1b5e32f445a07c215d4573
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-08 21:12:15 +0530

    Added: PartTime Employee and Calculate Wage

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -12,16 +12,20 @@ public class EmpWage {
      * If Absent Salary is returned 0
      */
     private static void calculateWage() {
-        double IS_PRESENT = 1;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double isPartTime = 1;
+        double isFullTime = 2;
+        double EmpRatePerHr = 20;
+        double EmpHrs;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
-        if (empCheck == IS_PRESENT) {
-            double EmpRatePerHr = 20;
-            double EmpHrs = 8;
-            CalculateSalary(EmpRatePerHr, EmpHrs);
+        if (empCheck == isFullTime) {
+            EmpHrs = 12;
+        } else if (empCheck == isPartTime) {
+            EmpHrs = 8;
         } else {
-            System.out.println("salary is 0");
+            EmpHrs = 0;
         }
+        CalculateSalary(EmpRatePerHr, EmpHrs);
     }
 
     /**
