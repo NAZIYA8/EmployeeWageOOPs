13d18511e3ef4ae0ce6777932ea08286d857e830
NAZIYA8 <syeda.naziya896@gmail.com>
2021-06-08 21:07:06 +0530

    Added: Check if Employee is Present

--- src/com/bridgelabz/EmpWage.java
+++ src/com/bridgelabz/EmpWage.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
 public class EmpWage {
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Employeee Wage computation");
+        ispresent();
+    }
+
+    private static void ispresent() {
+        double IS_PRESENT = 1;
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+
+        if (empCheck == IS_PRESENT) {
+            System.out.println("Employee is Present ");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
